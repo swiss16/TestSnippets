856cbb4ba7b994758d13c385f6857d59bf793c3b
Martin Osterwalder <swiss16@users.noreply.github.com>
2014-12-30 12:52:30 +0100

    html test projekt

--- PDFTest/PDFTest/MainWindow.xaml.cs
+++ PDFTest/PDFTest/MainWindow.xaml.cs
@@ -20,9 +20,15 @@ namespace PDFTest
     /// </summary>
     public partial class MainWindow : Window
     {
+        PdfGenerieren pdf = new PdfGenerieren();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            pdf.GenerierePDF();
         }
     }
 }
